efed3e4f644bb1d99a445681788ef9559a5c5f8f
o-marvel <osemekemrvlls@gmail.com>
2025-11-06 12:56:14 +0000

    assign start

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 10 
+number2 = 5
+print(f"addition of number1 and number2 is {number1 + number2}")
+print(f" substration of number1 and number2 is {number2 - number1}")
+print(f"multiplication of number1 and number2 is {number1 * number2}")
