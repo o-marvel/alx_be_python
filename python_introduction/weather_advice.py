555c32bd5ce04c4cae5f145d76b2f131805158b7
o-marvel <osemekemrvlls@gmail.com>
2025-11-13 13:19:23 +0000

    redo v1

--- python_introduction/weather_advice.py
+++ python_introduction/weather_advice.py
@@ -1,22 +1,11 @@
 
-# weather = input("What's the weather like today? (sunny/rainy/cold):").strip().lower()
-# if weather == 'sunny':
-#     print("Wear a t-shirt and sunglasses")
-# elif weather == 'rainy':
-#     print(" Don't forget your umbrella and a raincoat.")
-# elif weather == 'cold':
-#     print(" Make sure to wear a warm coat and a scarf.")
-# else:
-#     print(" Sorry, I don't have recommendations for this weather.")
+weather = input("What's the weather like today? (sunny/rainy/cold):").strip().lower()
+if weather == "sunny":
+    print("Wear a t-shirt and sunglasses")
+elif weather == "rainy":
+    print(" Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+    print(" Make sure to wear a warm coat and a scarf.")
+else:
+    print(" Sorry, I don't have recommendations for this weather.")
 
-weather = input("What is the weather like today? ").lower()
-
-match weather:
-    case "sunny":
-        print("Wear a t-shirt and sunglasses")
-    case "rainy":
-        print("Don't forget your umbrella and a raincoat.")
-    case "cold":
-        print("Make sure to wear a warm coat and a scarf.")
-    case _:
-        print("Sorry, I don't have recommendations for this weather.")
