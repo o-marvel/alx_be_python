0c63d78a23fb055ae41a5fa82899f8963e63de05
o-marvel <osemekemrvlls@gmail.com>
2025-11-13 14:47:03 +0000

    added weather adv pro

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,7 +1,3 @@
-# If the weather is “sunny”, recommend: Wear a t-shirt and sunglasses.
-# If the weather is “rainy”, recommend: Don't forget your umbrella and a raincoat.
-# If the weather is “cold”, recommend: Make sure to wear a warm coat and a scarf.
-# Include an else statement that handles unexpected input by printing: Sorry, I don't have recommendations for this weather.
 
 weather = input("What's the weather like today? (sunny/rainy/cold):").strip().lower()
 if weather == 'sunny':
