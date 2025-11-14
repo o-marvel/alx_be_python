4d780b7e4f597b89c457a6763fbaade727e94e41
o-marvel <osemekemrvlls@gmail.com>
2025-11-14 00:53:10 +0000

    mult-tabled add v1

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,5 @@
-number = int(input("Enter a number to generate its multiplication table: "))
+# number = int(input("Enter a number to generate its multiplication table: "))
+number = input("Enter a number to generate its multiplication table: ")
 for i in range(1, 11):  
     result = number * i
     print(f"{number} x {i} = {result}")
