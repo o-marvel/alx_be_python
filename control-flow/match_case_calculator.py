cc57c937256376ee0f2bacd976ff3eb4b071cb6c
o-marvel <osemekemrvlls@gmail.com>
2025-11-14 00:42:52 +0000

    matc_case done 2

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,19 +1,19 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-operation = input("Enter an operation (+, -, *, /): ").strip()
+operation = input("Choose the operation (+, -, *, /): ") 
 match operation:
     case '+':
-        result = num1 + num2
-        print(f"The result of {num1} {operation} {num2} is: {result}")
+        result = num1 + num2 
+        print(f"The result is: {result}.")
     case '-':
         result = num2 - num1
-        print(f"The result of {num1} {operation} {num2} is: {result}")
+        print(f"The result is: {result}.")
     case '*':
         result = num1 * num2 
-        print(f"The result of {num1} {operation} {num2} is: {result}")
+        print(f"The result is: {result}.")
     case '/':
         if num2 == 0 or num1 == 0:
-            print("Error:Cannot divide by zero")
+            print("Error:Cannot divide by zero")  
         else:
             result = num1 / num2
             print(f"The result of {num1} {operation} {num2} is: {result}")
