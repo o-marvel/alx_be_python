b96010a700fa637dac4363b031fe8dce5f6ca408
o-marvel <osemekemrvlls@gmail.com>
2025-11-14 01:11:49 +0000

    mult-update

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,6 @@
-# number = int(input("Enter a number to generate its multiplication table: "))
-number = input("Enter a number to generate its multiplication table: ")
+number = int(input("Enter a number to generate its multiplication table: "))
+ 
 for i in range(1, 11):  
-    print(f"{number} * {i} = {int(number) * i}")
+#   print(f"{number} * {i} = {int(number) * i}")
+ print(f"{number} * {i} = {int(number) * i}")
+
