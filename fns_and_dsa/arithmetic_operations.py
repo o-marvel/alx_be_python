28855d47c6f741c6a9fcfe2faa3e5aae07d02d80
o-marvel <osemekemrvlls@gmail.com>
2025-11-18 14:03:23 +0000

    week 5

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,28 @@
+
+
+def perform_operation():
+    """Performs a basic arithmetic operation based on user input."""
+    operation = input("Enter operation (+, -, *, /): ")
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if operation == '+':
+        result = num1 + num2
+        return result
+    elif operation == '-':
+        result = num1 - num2
+        return result
+    elif operation == '*':
+        result = num1 * num2
+        return result
+    elif operation == '/':
+        if num2 != 0:
+            result = num1 / num2
+            return result
+        else:
+            return "Error: Division by zero is not allowed."
+    else:
+        result =  "Error: Invalid operation."
+        return result
+
+# if __name__ == "__main__":
