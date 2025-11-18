c7b67f1e5a7e5a6994e912e6e83d3e80f0543194
o-marvel <osemekemrvlls@gmail.com>
2025-11-18 14:09:32 +0000

    week 5

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,10 +1,10 @@
 
 
-def perform_operation():
+def perform_operation(num1, num2, operation):
     """Performs a basic arithmetic operation based on user input."""
-    operation = input("Enter operation (+, -, *, /): ")
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+    # operation = input("Enter operation (+, -, *, /): ")
+    # num1 = float(input("Enter first number: "))
+    # num2 = float(input("Enter second number: "))
 
     if operation == '+':
         result = num1 + num2
