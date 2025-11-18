5e16ca33553131c044a26c96e539e5f7fa558fec
o-marvel <osemekemrvlls@gmail.com>
2025-11-18 15:55:46 +0000

    week5 redo

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,21 +1,21 @@
 
 
 def perform_operation(num1, num2, operation):
-    
+
     # operation = input("Enter operation (+, -, *, /): ")
     # num1 = float(input("Enter first number: "))
     # num2 = float(input("Enter second number: "))
 
-    if operation == '+':
+    if operation == 'add':
         result = num1 + num2
         return result
-    elif operation == '-':
+    elif operation == 'subtract':
         result = num1 - num2
         return result
-    elif operation == '*':
+    elif operation == 'multiply':
         result = num1 * num2
         return result
-    elif operation == '/':
+    elif operation == 'divide':
         if num2 != 0:
             result = num1 / num2
             return result
