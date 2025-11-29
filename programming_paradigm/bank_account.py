c3221a0c45b002c03539e146901bf6876cfeb060
o-marvel <osemekemrvlls@gmail.com>
2025-11-29 01:23:20 +0000

    addexe5

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,6 +15,7 @@ class BankAccount:
 
     # def display_balance(self):
     #     return f"Current Balance: ${self.__account_balance:.2f}"
-    
+
     def display_balance(self):
-        return f"Current Balance: ${self.__account_balance}"
+        print(f"Current Balance: ${self.__account_balance:.2f}")
+        
