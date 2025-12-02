f26336a35dac1e7b46c2048432272ec2f13821a7
o-marvel <osemekemrvlls@gmail.com>
2025-12-02 21:12:25 +0000

    ex7b

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,36 +1,28 @@
-# Attributes: title (str) and author (str).
-# Method: __init__(self, title, author) for initializing book attributes.
 class Book:
     def __init__(self, title, author):
-        self.title:str = title
-        self.author:str  = author
-
+        self.title = title
+        self.author = author
+    
     def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
-#  EBook and PrintBook:
 class EBook(Book):
-    def __init__(self, title, author, file_size):
+    def __init__(self,title, author, file_size):
         super().__init__(title, author)
-        self.file_size:int = file_size  # in MB
-        # print(f"EBook : {self.title} by {self.author} of size {file_size}MB")
-
+        self.file_size = file_size
+    
     def __str__(self):
-        return f"EBook: {self.title} by {self.author} of size {self.file_size}MB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
-        self.page_count:int = page_count # in kg
-
+        self.page_count = page_count
+    
     def __str__(self):
-       print(f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count} kg")
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
-
-#     Attributes: books (a list to store instances of Book, EBook, and PrintBook).
-# Methods:
-# add_book(self, book): Adds a Book, EBook, or PrintBook instance to the library.
-# list_books(self): Prints details of each book in the library.
+    
 class Library:
     def __init__(self):
         self.books = []
@@ -42,3 +34,4 @@ class Library:
         for book in self.books:
             print(book)
         return None
+    
